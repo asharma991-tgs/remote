c5b4f50d36964751f8b682cb7bb93aa3d81773bd
ankurtgs <108650977+ankurtgs@users.noreply.github.com>
2023-04-24 13:28:35 +0530

    feat: update config to support es2016

--- src/Button.jsx
+++ src/Button.jsx
@@ -2,15 +2,17 @@ import { useState } from "react";
 
 import "./Button.css";
 
-export const Button = () => {
+const Button = () => {
   const [state, setState] = useState(0);
   return (
     <div>
-      <button className='shared-btn' onClick={() => setState((s) => s + 1)}>
+      <button className="shared-btn" onClick={() => setState((s) => s + 1)}>
         Click me: {state}
       </button>
     </div>
   );
 };
+
+export const Game = () => true;
 
 export default Button;
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    topLevelAwait({
+      // The export name of top-level await promise for each chunk module
+      promiseExportName: "__tla",
+      // The function to generate import names of top-level await promise in each chunk module
+      promiseImportName: (i) => `__tla_${i}`,
+    }),
     federation({
       name: "remote",
       filename: "remoteEntry.js",
@@ -17,7 +24,7 @@ export default defineConfig({
   ],
   build: {
     modulePreload: false,
-    target: "esnext",
+    target: "es2016",
     minify: false,
     cssCodeSplit: false,
   },
